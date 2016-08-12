cb171e0c00983c06825e6dee491a2202fe31be62
pythonex <harrigenius19@gmail.com>
2016-08-12 10:04:45 +0300

    example-5 using generics

---
+++ example-5/views.py
@@ -0,0 +1,19 @@
+from rest_framework import status
+from rest_framework import generics
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+class SnippetList(generics.ListCreateAPIView):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
