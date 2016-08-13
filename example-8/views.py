44b4a8f512a216a5fab64cffe487504550ec13e4
pythonex <harrigenius19@gmail.com>
2016-08-13 11:57:06 +0300

    using ViewSet

---
+++ example-8/views.py
@@ -0,0 +1,41 @@
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+from rest_framework.response import Response
+
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+
+from django.contrib.auth.models import User
+
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+		'users':reverse('user-list', request=request, format=format),
+		'snippets':reverse('snippet-list', request=request, format=format),
+	    })
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+	snippet = self.get_object()
+	return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+	serializer.save(owner=self.request.user)
